b830e052f0af2e9ae24d8d2dcb4eb4a0e7e48646
Nicholas Amarillo <nicholasamarillo@gmail.com>
2023-03-04 23:18:18 -0600

    added structure of project

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,3 @@
-const myKey = "d1603ebd153df14e914717e51ca63fee";
-const searchedCity = document.getElementsByClassName('searchedCity');
-const searchButton = document.getElementsByClassName('btn');
-
-function setQuery(){
-    if (searchButton == true){
-        getAPI(searchedCity.value);
-        console.log(searchedCity.value);
-    }
-}
-
-function getAPI(){
-    const requestURL = `https://api.openweathermap.org/data/2.5/weather?lat=${searchedCity}&lon=${searchedCity}&appid=${myKey}`;
-
-    fetch(requestURL)
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
-
-
-setQuery();
-getAPI();
+for (let i = 0; i<12; i++){
+    console.log("hello world")
+};
