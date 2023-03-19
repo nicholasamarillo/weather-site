a1dd4d98923ad4c82b84dd357a24bb8a079ff98f
Nicholas Amarillo <nicholasamarillo@gmail.com>
2023-03-18 19:02:09 -0500

    finished main card, added all the corresponding emojis, changed font.

--- javascript/index.js
+++ javascript/index.js
@@ -15,7 +15,50 @@ async function checkWeather(city){
     var data = await res.json();
     console.log(data);
 
-    
+    document.querySelector("#city-name").innerHTML = data.name;
+    document.querySelector("#weather-style").innerHTML = data.weather[0].description;
+    document.querySelector("#current-weather").innerHTML = data.main.temp + "°F";
+    document.querySelector("#temp-high").innerHTML = data.main.temp_max + "°F";
+    document.querySelector("#temp-low").innerHTML = data.main.temp_min + "°F";
+
+    if (data.weather[0].main == "Clear"){
+        document.querySelector("#weather-emoji").innerHTML = "☀️";
+    };
+    if (data.weather[0].description == "few clouds"){
+        document.querySelector("#weather-emoji").innerHTML = "⛅️";
+    };
+    if (data.weather[0].description == "overcast clouds"){
+        document.querySelector("#weather-emoji").innerHTML = "☁️";
+    };
+    if (data.weather[0].description == "tornado"){
+        document.querySelector("#weather-emoji").innerHTML = "🌪️";
+    };
+    if (data.weather[0].description == "fog"){
+        document.querySelector("#weather-emoji").innerHTML = "🌫️";
+    };
+    if (data.weather[0].description == "mist"){
+        document.querySelector("#weather-emoji").innerHTML = "🌫️";
+    };
+    if (data.weather[0].description == "moderate rain"){
+        document.querySelector("#weather-emoji").innerHTML = "🌧️";
+    };
+    if (data.weather[0].description == "light rain"){
+        document.querySelector("#weather-emoji").innerHTML = "🌧️";
+    };
+    if (data.weather[0].main == "rain"){
+        document.querySelector("#weather-emoji").innerHTML = "🌧️";
+    };
+    if (data.weather[0].main == "drizzle"){
+        document.querySelector("#weather-emoji").innerHTML = "🌦️";
+    };
+    if (data.weather[0].main == "thunderstorm"){
+        document.querySelector("#weather-emoji").innerHTML = "⛈️";
+    };
+    if (data.weather[0].main == "snow"){
+        document.querySelector("#weather-emoji").innerHTML = "🌨️";
+    };
+
+
 };
 
 searchBtn.addEventListener("click", ()=>{
